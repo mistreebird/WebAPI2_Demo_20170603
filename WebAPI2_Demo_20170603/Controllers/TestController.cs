70c1467a0c13aca58f24425ff3fbcf2d59f84e4d
KaneWei <mistreebird@gmail.com>
2017-06-07 14:12:01 +0800

    10 練習模型繫結方法

---
+++ WebAPI2_Demo_20170603/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI2_Demo_20170603.Controllers
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+    public class TestController : ApiController
+    {
+        [Route("Person1")]
+        public IHttpActionResult GetPerson1(string name,int age)
+        {
+            return Ok(name + "-" + age);
+        }
+
+        [Route("Person2")]
+        public IHttpActionResult PostPerson2(string name,[FromBody] int age)
+        {
+            return Ok(name + "-" + age);
+        }
+
+        [Route("Person3")]
+        public IHttpActionResult PostPerson3(Person person)
+        {
+            return Ok(person.name + "-" + person.age);
+        }
+
+        [Route("Person4")]
+        public IHttpActionResult PostPerson4([FromUri] Person person)
+        {
+            return Ok(person.name + "-" + person.age);
+        }
+    }
+}
